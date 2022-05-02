2ef425f3ec532684d1b1fc9266d47d5258fe4213
Seth Martin <jahgpup@gmail.com>
2022-05-02 14:10:28 -0600

    gotta stop over thinking these easy ones

---
+++ 5:2:22OppositeNumber/js/main.js
@@ -0,0 +1,8 @@
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+    //should be easy
+    return -number;
+  }
+
+  //My first one didn't work but was way over thinking it
