b29185040891f6e6b584de4fd751b1c0870c86ac
Seth Martin <jahgpup@gmail.com>
2022-05-11 10:45:29 -0600

    sometimes the math is way more simple than I originally think

---
+++ 5:11:22BeginnerSeries/js/solution001.js
@@ -0,0 +1,7 @@
+//There are 3600 seconds in an hour
+
+function past(h, m, s){
+    return h*360000 + m*60000 + s*1000
+}
+
+//was thinking way too hard about thi one. No iterations of conditionals necessary, but let's see if they will work
---
+++ 5:11:22BeginnerSeries/js/solution002.js
@@ -0,0 +1,5 @@
+//Could have just multiplied everything by 1000 afterwards
+
+function past(h, m, s){
+    return ((h*3600)+(m*60)+s)*1000;
+  }
