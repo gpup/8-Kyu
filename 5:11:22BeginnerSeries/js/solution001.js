8c42d4802981e0d5fc57e54fd80d321d0b322ba6
Seth Martin <jahgpup@gmail.com>
2022-05-11 10:51:57 -0600

    sometimes the math is much simpler than I initially think

--- 5:11:22BeginnerSeries/js/solution001.js
+++ 5:11:22BeginnerSeries/js/solution001.js
@@ -4,4 +4,4 @@ function past(h, m, s){
     return h*360000 + m*60000 + s*1000
 }
 
-//was thinking way too hard about thi one. No iterations of conditionals necessary, but let's see if they will work
+//was thinking way too hard about thi one. No iterations of conditionals necessary, but let's see if they will work 
