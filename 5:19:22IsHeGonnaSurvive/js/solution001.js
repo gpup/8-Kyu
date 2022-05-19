7389bc583d3c126b5601bcac3a0fa4cefec2872c
Seth Martin <jahgpup@gmail.com>
2022-05-19 12:31:17 -0500

    sometimes you need to pull out a pen and paper and give the math a second thought. such a simple mistake

---
+++ 5:19:22IsHeGonnaSurvive/js/solution001.js
@@ -0,0 +1,11 @@
+//I guess if dragons is >= bullets*2 return true
+
+function hero(bullets, dragons){
+  if(dragons % bullets*2 >= 0){
+    return true
+  }else
+  return false
+}
+
+
+//What was I thinking? Do I just not know how to do math?
---
+++ 5:19:22IsHeGonnaSurvive/js/solution002.js
@@ -0,0 +1,8 @@
+//I mean come on man
+
+function hero(bullets, dragons){
+    if(bullets/2 >= dragons){
+      return true
+    }else 
+      return false
+  }
