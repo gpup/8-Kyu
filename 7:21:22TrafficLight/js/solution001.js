a2af04e604b24f4e7fd313481bc5d2e9744e5a77
Seth Martin <jahgpup@gmail.com>
2022-07-21 10:51:54 -0600

    real easy

---
+++ 7:21:22TrafficLight/js/solution001.js
@@ -0,0 +1,11 @@
+//Nice
+
+function updateLight(current) {
+    if(current === 'green'){
+      return 'yellow'
+    }else if(current === 'yellow'){
+      return 'red'
+    }else{
+      return 'green'
+    }
+  }
