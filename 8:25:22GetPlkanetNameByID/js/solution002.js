04100b9d6d2344255d9994c20f281a8b6ccbedc8
Seth Martin <jahgpup@gmail.com>
2022-08-25 20:24:30 -0600

    this one was really stupid

---
+++ 8:25:22GetPlkanetNameByID/js/solution002.js
@@ -0,0 +1,14 @@
+//another stupid way to do this stupid one
+
+function getPlanetName(id){
+    return {
+      1: 'Mercury',
+      2: 'Venus',
+      3: 'Earth',
+      4: 'Mars',
+      5: 'Jupiter',
+      6: 'Saturn',
+      7: 'Uranus',
+      8: 'Neptune'
+    }[id]
+  }
